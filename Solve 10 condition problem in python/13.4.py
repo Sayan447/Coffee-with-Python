2ba94466d908b5f6651316c67b5cae44d06ad6f5
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-06 20:56:20 +0530

    4th probelm

---
+++ Solve 10 condition problem in python/13.4.py
@@ -0,0 +1,17 @@
+#Weather Activity Suggestion
+# problem :- Suggest an activity based on the weather(e.g., Sunny - go for a walk, rainy - read a book, snowy - build a snowman)
+
+# activity = input("enter the value ")
+# if activity == "sunny":print("go for a walk")
+# elif activity == "rainy":print("read a book")
+# elif activity == "snowy": print("build a snowman")
+
+weather = "Sunny"
+if weather == "Sunny":
+    activity = "go for a walk"
+elif weather == "rainy":
+    activity = "read a book"
+elif weather == 'snowy':
+    activity = "build a snowman"
+    
+print(activity)
