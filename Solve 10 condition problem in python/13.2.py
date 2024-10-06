675f01cc6e4ee2bcdda7410a31bf20b197cb377f
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-06 20:15:43 +0530

    1st comment

--- Solve 10 condition problem in python/13.2.py
+++ Solve 10 condition problem in python/13.2.py
@@ -2,6 +2,9 @@
 #problem:-Assign a letter grade based on a student's score: A(90-100),B(80-89),C(70-79),D(60-69),F(below 60).
 
 grade = int(input("enter the number: "))
+if grade>=100:
+    print("please check your number")
+    exit()
 if grade >= 90:
     print("A")
 elif grade >= 80 and grade <= 89:
