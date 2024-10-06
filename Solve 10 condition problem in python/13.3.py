bd1176a5c779b759c2a0085acf0e9f15ea419c76
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-06 20:47:19 +0530

    3rd prblem solve

---
+++ Solve 10 condition problem in python/13.3.py
@@ -0,0 +1,9 @@
+#Determine if a fruit is ripe, overripe,or unripe based on it's color(e.g., Banana:green - unripe, Yellow - ripe, Brown - Overripe)
+
+color = input("enter the color: ")
+if color == "green":
+    print('Unripe')
+elif color == "Yellow":
+    print("Ripe")
+elif color == "Brown":
+    print("Overripe")
