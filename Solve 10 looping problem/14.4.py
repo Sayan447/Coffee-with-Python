9b9bd64ae9f9bdb634a3c9f5cda6519a1f4e751a
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-20 01:16:48 +0530

    4th probelm solve

---
+++ Solve 10 looping problem/14.4.py
@@ -0,0 +1,11 @@
+# Reverse a string using a loop
+# n = "SAYAN"
+# print(n[::-1])
+
+
+inp = input("")
+reverse = ""
+for char in inp:
+    print(char)
+    reverse = char + reverse 
+print(reverse)
